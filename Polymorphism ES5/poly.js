3601d1065ec376a90b82407bd18e57635d0f3be8
ordinary-zahid <mjhasan38@gmail.com>
2021-08-29 09:34:45 +0600

    Polymorphism ES5

---
+++ Polymorphism ES5/poly.js
@@ -0,0 +1,64 @@
+
+
+const Animal = function(name){
+
+    this.name = name;
+    
+    this.canWalk = function(){
+
+       console.log(`${this.name} can walk`);
+    };
+
+};
+
+Animal.prototype = {
+
+ canSound: function(){
+
+     console.log(`Animal common sound`);
+  }
+
+}
+
+const Cat = function(name){
+
+     //this.name = name;
+     Animal.call(this, name);
+
+};
+
+Cat.prototype = Object.create(Animal.prototype);
+Cat.prototype.constructor = Cat;
+
+Cat.prototype.canSound = function(){
+
+    console.log('This is cat sound miaw miaw');
+}
+
+const Dog = function(name){
+
+    this.name = name;
+
+};
+
+Dog.prototype = Object.create(Animal.prototype);
+Dog.prototype.constructor = Dog;
+
+Dog.prototype.canSound = function(){
+
+    console.log('This is Dog sound ghew ghew');
+}
+
+const a1 = new Animal('Tiger');
+const a2 = new Animal('Lion');
+const c1 = new Cat('miny');
+const d1 = new Dog('kalu');
+
+
+const obj = [a1, c1, d1];
+
+for(i of obj){
+    i.canSound();
+}
+
+
